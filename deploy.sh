12ccb83f0247b2fded711286e5e638c08b0ce40e
Chris Snyder <bumishness@gmail.com>
2013-10-21 13:27:40 -0500

    Undo change to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 rm -rf build
 haw build
-s3cmd put --acl-public --recursive put build/ s3://radio.galaxyzoo.org/beta-chris/
-# s3cmd put --acl-public --recursive put build/ s3://radio.galaxyzoo.org/beta2/
+s3cmd put --acl-public --recursive put build/ s3://radio.galaxyzoo.org/beta2/
 # s3cmd put --acl-public put build/index.html s3://radio.galaxyzoo.org/beta2/
 # s3cmd put --acl-public put build/application* s3://radio.galaxyzoo.org/beta2/
 # s3cmd put --acl-public put build/workers/conrec.js s3://radio.galaxyzoo.org/beta2/workers/
