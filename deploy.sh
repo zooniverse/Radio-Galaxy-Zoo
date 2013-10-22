e797dc9e92363caafaf91c0c078d1f7a7bdb94d0
Amit Kapadia <amit@zooniverse.org>
2013-10-22 12:13:57 -0500

    gzip js and css

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 rm -rf build
 haw build
-s3cmd put --acl-public --recursive put build/ s3://radio.galaxyzoo.org/beta2/
-# s3cmd put --acl-public put build/index.html s3://radio.galaxyzoo.org/beta2/
-# s3cmd put --acl-public put build/application* s3://radio.galaxyzoo.org/beta2/
-# s3cmd put --acl-public put build/workers/conrec.js s3://radio.galaxyzoo.org/beta2/workers/
+gzip -9 build/application*
+
+for f in build/*.gz;
+do
+  mv $f ${f%.gz} 
+done
+
+s3cmd put --acl-public --mime-type "application/javascript" --add-header "Content-Encoding: gzip" build/application*.js s3://radio.galaxyzoo.org/beta2/
+s3cmd put --acl-public --mime-type "text/css" --add-header "Content-Encoding: gzip" build/application*.css s3://radio.galaxyzoo.org/beta2/
+s3cmd put --acl-public put build/favicon.ico s3://radio.galaxyzoo.org/beta2/
+s3cmd put --acl-public --recursive put build/images/ s3://radio.galaxyzoo.org/beta2/images/
+s3cmd put --acl-public --recursive put build/workers/ s3://radio.galaxyzoo.org/beta2/workers/
