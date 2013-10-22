20a2578fa4510acf1b431b85aae7928d2e57cdda
Amit Kapadia <amit@zooniverse.org>
2013-10-22 12:44:12 -0500

    remove d3 from setup script

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ curl -k "http://code.angularjs.org/1.2.0-rc.3/angular-route.js" -o "app/lib/angu
 curl -k "https://raw.github.com/astrojs/fitsjs/8721bba7f018f8163fbd2b07364c496a8ee77bda/lib/fits.js" -o "app/lib/fits.js"
 curl -k "https://raw.github.com/jasondavies/conrec.js/361f86be3b1a2c46a228233d6e902926ca530213/conrec.js" -o "app/lib/conrec.js"
 curl -k "https://raw.github.com/fryn/html5slider/09d19a1067ada41eb3e74cb011f9fd237117dc74/html5slider.js" -o "app/lib/html5slider.js"
-curl -k "https://raw.github.com/mbostock/d3/v3.2.8/d3.js" -o "app/lib/d3.js"
 
 curl -k "https://raw.github.com/twbs/bootstrap/v3.0.0/dist/css/bootstrap.css" -o "css/bootstrap.css"
 curl -k "https://github.com/zooniverse/Zooniverse/blob/959f413dd2493a9a3cdb6bcdc4e9e184aad123a2/css/dialog.css" -o "css/zooniverse/dialog.css"
